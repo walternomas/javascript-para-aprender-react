319618a07caa045785243f05bdc21c6c26d40cbf
walternomas <w.barriosvazquez@gmail.com>
2022-08-13 16:35:47 -0300

    asincronismo: fetch, promesas

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
-const person = {
-  name: 'ryan',
-  address: {
-    city: 'london'
-  }
-};
+let data = fetch('https://jsonplaceholder.typicode.com/posts');
 
-console.log(person.address.city);
-//Optional Chaining
-console.log(person.location?.city);
+console.log(data);
+
+const ul = document.createElement('ul');
+//const li = document.createElement('li');
+
+fetch('https://jsonplaceholder.typicode.com/posts')
+  .then(function (response) {
+    console.log('finalizó la carga');
+    console.log(response);
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+    data.forEach(post => {
+      const li = document.createElement('li');
+      li.innerText = post.title;
+      ul.append(li);
+    });
+    document.body.append(ul);
+  })
+
+console.log('linea 2');
