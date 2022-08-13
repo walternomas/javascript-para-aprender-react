3bfa7d98bd31c6e594b5764d2e5df83e7eafb937
walternomas <w.barriosvazquez@gmail.com>
2022-08-13 13:02:18 -0300

    Objects: properties and methods

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
-function add(x, y=0) {
-  return x + y;
+const user = {
+  name: 'walter',
+  lastname: 'barrios',
+  age: 44,
+  address: {
+    country: 'argentina',
+    city: 'buenos aires',
+    street: 'falsa 123'
+  },
+  friends: ['martin', 'jorgelina'],
+  active: true,
+  sendMail: function() {
+    return 'sending email...'
+  },
+  sendMail2() {
+    return 'sending other email...'
+  }
 }
 
-console.log(add(10));
-console.log(add(10,5));
+console.log(user);
+console.log(user.name);
+console.log(user.address);
+console.log(user.address.city);
+console.log(user.friends);
+console.log(user.active);
+console.log(user.sendMail);
+console.log(user.sendMail());
