5b28b82793025b921208d2bb64d11db40037d64b
walternomas <w.barriosvazquez@gmail.com>
2022-08-13 15:52:39 -0300

    spread operator

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
-//Array methods
-const names = ['ryan', 'joe', 'maria', 'joe'];
+//spread operator
+const names = ['ryan', 'joe', 'maria'];
 const newNames = ['marcos', 'mario', 'john'];
 
-console.log(names.concat(newNames));
-console.log(names);
-console.log(newNames);
+console.log([...names, ...newNames]);
+
+const user = {
+  name: 'ryan',
+  lastname: 'ray'
+}
+
+const address = {
+  street: 'main street 123',
+  city: 'buenos aires'
+}
+
+const userInfo = {
+  ...user,
+  ...address
+}
+
+console.log(userInfo);
+console.log(user);
+console.log(address);
