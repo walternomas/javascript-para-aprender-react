1c66b9a0d77eece1e793e90b1a885d9be222290e
walternomas <w.barriosvazquez@gmail.com>
2022-08-13 13:05:47 -0300

    shorthand property names

--- index.js
+++ index.js
@@ -1,27 +1,15 @@
-const user = {
-  name: 'walter',
-  lastname: 'barrios',
-  age: 44,
-  address: {
-    country: 'argentina',
-    city: 'buenos aires',
-    street: 'falsa 123'
-  },
-  friends: ['martin', 'jorgelina'],
-  active: true,
-  sendMail: function() {
-    return 'sending email...'
-  },
-  sendMail2() {
-    return 'sending other email...'
-  }
-}
+//Shorthand property names
+const name = 'laptop';
+const price = 3000;
 
-console.log(user);
-console.log(user.name);
-console.log(user.address);
-console.log(user.address.city);
-console.log(user.friends);
-console.log(user.active);
-console.log(user.sendMail);
-console.log(user.sendMail());
+// const newProduct = {
+//   name: name,
+//   price: price
+// };
+
+const newProduct = {
+  name,
+  price
+};
+
+console.log(newProduct);
