84de8f224517bf150855de5ccb5ab084b935b508
walternomas <w.barriosvazquez@gmail.com>
2022-08-13 15:48:00 -0300

    array methods: concat

--- index.js
+++ index.js
@@ -1,32 +1,7 @@
 //Array methods
 const names = ['ryan', 'joe', 'maria', 'joe'];
+const newNames = ['marcos', 'mario', 'john'];
 
-// for (let i = 0; i < names.length; i++) {
-//   const element = names[i];
-//   console.log(element);
-// }
-names.forEach(name => {
-  console.log(name);
-});
-
-// const newNames = names.map(function (name) {
-//   return `Hello ${name}`;
-// });
-const newNames = names.map(name => `Hello ${name}`);
+console.log(names.concat(newNames));
+console.log(names);
 console.log(newNames);
-console.log(names);
-
-const nameFound = names.find(function (name) {
-  if(name === 'joe') {
-    return name;
-  }
-});
-console.log(nameFound);
-
-const nameFilter = names.filter(function (name) {
-  if(name !== 'joe') { //elimino los joes
-    return name;
-  }
-});
-console.log(nameFilter);
-console.log(names);
