dd71f299800abd92396277d4e161dd55fc2feaaa
walternomas <w.barriosvazquez@gmail.com>
2022-08-13 12:49:18 -0300

    function parameters

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-function hello() {
-  return function() {
-    return 'hola mundo'
-  }
+function hello(name) {
+  return 'hola ' + name;
 }
 
-console.log(hello()());
+console.log(hello('walter'));
+console.log(hello('omar'));
+
+function add(x, y) {
+  return x + y;
+}
+
+console.log(add(10,20));
+console.log(add(4,5));
+console.log(add(10,7));
