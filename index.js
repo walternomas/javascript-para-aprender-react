ea4b26d04edf30ad7140a74d0d4bc5588aca873d
walternomas <w.barriosvazquez@gmail.com>
2022-08-13 16:43:19 -0300

    asincronismo: async, await

--- index.js
+++ index.js
@@ -1,24 +1,14 @@
-let data = fetch('https://jsonplaceholder.typicode.com/posts');
-
-console.log(data);
-
 const ul = document.createElement('ul');
-//const li = document.createElement('li');
 
-fetch('https://jsonplaceholder.typicode.com/posts')
-  .then(function (response) {
-    console.log('finalizó la carga');
-    console.log(response);
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
-    data.forEach(post => {
-      const li = document.createElement('li');
-      li.innerText = post.title;
-      ul.append(li);
-    });
-    document.body.append(ul);
-  })
+async function loadData() {
+  const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const data = await response.json();
+  data.forEach(post => {
+    const li = document.createElement('li');
+    li.innerText = post.title;
+    ul.append(li);
+  });
+  document.body.append(ul);
+}
 
-console.log('linea 2');
+loadData();
